c02c760ed009d146279c25c6a1bf283f630f2445
Quentin Duff <qcduff@btinternet.com>
2023-02-04 20:53:22 +0000

    fix static

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -1,6 +1,7 @@
 from quart import Blueprint, send_file, current_app
 
-webbp = Blueprint('web', __name__, static_folder='./static')
+webbp = Blueprint('web', __name__, static_folder='./static',
+                  static_url_path="")
 
 
 @webbp.route('/')

7ceca53b22d2f402daea06d3fcb4c4039ae59b53
Quentin Duff <qcduff@btinternet.com>
2023-02-04 23:46:39 +0000

    i dont even knwo

--- app/web/static/mapping.js
+++ app/web/static/mapping.js
@@ -4,6 +4,7 @@ window.onload = buildMap();
 // INSERT BACKEND URL
 var backEndUrl = "";
 
+
 // adds tile layers
 function buildMap() {
     var map = L.map('map').setView([51.505, -0.09], 13);
@@ -18,7 +19,7 @@ function buildMap() {
 
 // GET requesets
 async function getDropOffBoxes(map) {
-    const response = await fetch( backEndUrl + '/user/create');
+    const response = await fetch(backEndUrl + '/user/create');
     const listOfDropboxes = await response.json();
     var pointList = [];
     for (let i = 0; i < listOfDropboxes.length; i++) {
@@ -42,15 +43,15 @@ function buildLocations(circleData, pointData, map) {
 }
 
 function buildDummy(map) {
-    var circleData = [[51.53, -0.092, true, 500],[51.54, -0.091, false, 500]]
-    var pointData = [[51.53, -0.092, true, "Post Box"],[51.54, -0.091, false, "Drop Point"]]
+    var circleData = [[51.53, -0.092, true, 500], [51.54, -0.091, false, 500]]
+    var pointData = [[51.53, -0.092, true, "Post Box"], [51.54, -0.091, false, "Drop Point"]]
     buildLocations(circleData, pointData, map);
 }
 
 function addCircle(xCoord, yCoord, destinationBool, radius) {
     var colour;
     var fillColour;
-    if (destinationBool) { 
+    if (destinationBool) {
         colour = '#1ABC9C';
         fillColour = '#A3E4D7';
     } else {
@@ -66,8 +67,8 @@ function addCircle(xCoord, yCoord, destinationBool, radius) {
     });
 }
 
-function addDropOffBox(xCoord, yCoord, destinationBool){
-    if (destinationBool) { 
+function addDropOffBox(xCoord, yCoord, destinationBool) {
+    if (destinationBool) {
         colour = '#1ABC9C';
     } else {
         colour = '#16A085';
@@ -77,4 +78,25 @@ function addDropOffBox(xCoord, yCoord, destinationBool){
         fillColor: colour,
         radius: 5
     });
+}
+
+
+flipped = false;
+
+function flip() {
+
+    elem = document.getElementById("secondary");
+    if (!flipped) {
+        console.log("shrink")
+
+        elem.style.width = "0px";
+    } else {
+        console.log("expand")
+        elem.style.width = "50%";
+    }
+    flipped = !flipped
+    map.invalidateSize();
+
+    document.getElementById("secondary").style.width = "0px";
+
 }

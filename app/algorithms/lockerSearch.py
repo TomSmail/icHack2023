3dd7bc692ead5830155d48656bebaf7592719a96
Luke Moran <luke.moran2004@gmail.com>
2023-02-04 21:47:50 +0000

    Added route_parcels for api frontend interaction

--- app/algorithms/lockerSearch.py
+++ app/algorithms/lockerSearch.py
@@ -14,16 +14,11 @@ class Graph:
 
     MAX_DAYS = 99999999
     def __init__(self):
-
         self.edges = defaultdict(list)
         self.parents = defaultdict(lambda: -1)
 
     def add_edge(self, u, v, start_dt : datetime.time, duration):
-
         self.edges[u].append(Edge(v, start_dt, duration))
-
-
-
 
     def find_best(self, start_id, end_id):
         # TODO - Fix when to start date, at the moment just 04/01/2023 at 9am
@@ -67,10 +62,6 @@ class Graph:
                     self.parents[edge.end_id] = (next_node, earliest_arrival, next_time)
                     best[edge.end_id] = earliest_arrival
                     hp.heappush(prioq, (earliest_arrival, edge.end_id))
-                #print("CURRENT TIME", cur_time)
-                #print(earliest_arrival, "FROM", next_node, "TO", edge.end_id)
-
-
 
         return best[end_id]
 
@@ -96,55 +87,65 @@ def route_parcel(start_node_id, end_node_id, edgeList):
 
     g = Graph()
     for (start_id, end_id, stime, duration) in edgeList:
-        print(type(stime))
+        #print(type(stime))
+        assert type(stime) == datetime.time
+        assert type(duration) == datetime.timedelta
+
+        g.add_edge(start_id, end_id, stime, duration)
+
+    res = g.find_best(start_node_id, end_node_id)
+    path = g.traceback(start_node_id, end_node_id)
+
+    return res, path
+
+
+if __name__ == "__main__":
+    #testdate = datetime.datetime(2023, 1, 4, 9, 3, 1)
+
+    #print(route_parcel(0,1, [(0,1, testdate.time(), datetime.timedelta(hours = 2))]))
+
+    tgraph = Graph()
 
 
 
-print(route_parcel(0,1, [(0,1, datetime.datetime)]))
+    edge1time = datetime.time(9) ### 9am today
+    edge1td = datetime.timedelta(hours=2)
 
-"""
-tgraph = Graph()
-
-
-
-edge1time = datetime.time(9) ### 9am today
-edge1td = datetime.timedelta(hours=2)
-
-tgraph.add_edge(1,2, edge1time, edge1td)
+    tgraph.add_edge(1,2, edge1time, edge1td)
 
 
-edge2time = datetime.time(23) ### 9pm today
-edge2td = datetime.timedelta(hours=3)
+    edge2time = datetime.time(23) ### 9pm today
+    edge2td = datetime.timedelta(hours=3)
 
-tgraph.add_edge(2,5, edge2time, edge2td)
+    tgraph.add_edge(2,5, edge2time, edge2td)
 
 
 
-edge3time = datetime.time(10) #10am
-edge3td = datetime.timedelta(hours=1)
+    edge3time = datetime.time(10) #10am
+    edge3td = datetime.timedelta(hours=1)
 
-tgraph.add_edge(1,3, edge3time, edge3td)
+    tgraph.add_edge(1,3, edge3time, edge3td)
 
-edge4time = datetime.time(9)
-edge4td = datetime.timedelta(hours=1)
+    edge4time = datetime.time(9)
+    edge4td = datetime.timedelta(hours=1)
 
-tgraph.add_edge(3,4, edge4time, edge4td)
-
-
-edge5time = datetime.time(10)
-edge5td = datetime.timedelta(hours=3)
-
-tgraph.add_edge(1,4, edge5time, edge5td)
-
-edge6time = datetime.time(14)
-edge6td = datetime.timedelta(hours=3)
-
-tgraph.add_edge(4,5, edge6time, edge6td)
+    tgraph.add_edge(3,4, edge4time, edge4td)
 
 
+    edge5time = datetime.time(10)
+    edge5td = datetime.timedelta(hours=3)
 
-print(tgraph.find_best(1, 5))
-tgraph.traceback(1, 5)"""
+    tgraph.add_edge(1,4, edge5time, edge5td)
+
+    edge6time = datetime.time(14)
+    edge6td = datetime.timedelta(hours=3)
+
+    tgraph.add_edge(4,5, edge6time, edge6td)
+
+
+
+    print(tgraph.find_best(1, 5))
+    res = tgraph.traceback(1, 5)
 
 
 

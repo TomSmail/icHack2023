968f464320e8bcfb3c4c7ceb10b134926ade5541
Tom Underhill <tom.underhill22@imperial.ac.uk>
2023-02-04 22:27:55 +0000

    adds classes

--- app/algorithms/findAdjacentNodes.py
+++ app/algorithms/findAdjacentNodes.py
@@ -22,10 +22,10 @@ def getAdjNodesDict(journeys, nodes):
     # goes through each journey each person makes
     for journey in journeys:
         # goes through each locker a person could start at
-        for startNode in pointNodesDict[journey.startLoc]:
+        for startNode in pointNodesDict[journey.startLocation]:
             startNodeAdjNodes = []
             # goes through each locker a person could go to
-            for endNode in pointNodesDict[journey.endLoc]:
+            for endNode in pointNodesDict[journey.endLocation]:
                 # adds the nodes that are adjacent to the start nodes to a list
                 # which is then added to the dictionary later
                 assert type(journey.startTime) == datetime.datetime
@@ -60,33 +60,41 @@ def nearBy(p1, p2, dist):
     return distance.distance((p1.lat, p1.long), (p2.lat, p2.long)).meters <= dist
 
 
-# import dataclasses
-# @dataclasses.dataclass
-#
-# class Node:
-#     pass
-#
-#
-# @dataclasses.dataclass
-# class Point:
-#     lat : float
-#     long: float
-#
-#
-# t1 = Point(52.1951, 0.1313)
-# t2 = Point(51.5072, 0.1276)
-#
-#
-#
-# print(type(datetime.datetime(2023, 1, 4, 12, 2, 1).time()) is datetime.time)
-#
-# print(nearBy(t1,t2,0)) ### About 75km away as expected
-#
-#
-#
-# ## datetime.time (h,m,s)
-# ## datetime.datetime (yy, m, d) h, m, s t1-t2 timedelta object
-#
+import dataclasses
+@dataclasses.dataclass
+class Point:
+    lat: float
+    long: float
+
+@dataclasses.dataclass
+class Node:
+    location: Point
+    id: int
+
+@dataclasses.dataclass
+class Journey:
+    startTime: datetime.datetime
+    endTime: datetime.datetime
+    startLocation: Point
+    endLocation: Point
+
+
+
+
+t1 = Point(52.1951, 0.1313)
+t2 = Point(51.5072, 0.1276)
+
+
+
+print(type(datetime.datetime(2023, 1, 4, 12, 2, 1).time()) is datetime.time)
+
+print(nearBy(t1,t2,0)) ### About 75km away as expected
+
+
+
+## datetime.time (h,m,s)
+## datetime.datetime (yy, m, d) h, m, s t1-t2 timedelta object
+
 
 
 

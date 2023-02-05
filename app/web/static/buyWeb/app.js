9b94d4c720e4b2867f186d2f01a48c3bfd609073
Hamish Starling <hamishstarling@hotmail.co.uk>
2023-02-05 07:48:40 +0000

    Update buy web

--- app/web/static/buyWeb/app.js
+++ app/web/static/buyWeb/app.js
@@ -1,6 +1,33 @@
 const api_url ="http://127.0.0.1:2025/";
   
 // Defining async function
+
+async function buy() {
+    const data = {
+         "start_locker": 6,
+         "end_locker": 7  
+    } 
+    
+    const response = await fetch(api_url + "api/producer/parcel/create", {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+            'Content-Type': 'application/json'
+            // 'Content-Type': 'application/x-www-form-urlencoded',
+          },
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+      });
+     
+    if (response.ok) {
+        alert("The item was purchased!")
+    }
+    else {
+        alert("An error occurred in purchasing the item.")
+    }
+
+}
+
+
 async function getapi(url) {
     
     // Storing response
